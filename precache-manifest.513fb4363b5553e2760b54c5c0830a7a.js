b3cf21f7301a9954f31e4ad5fc5e4f7107de1806
Julian Rudolph <julianrudolph@Julians-MacBook-Air.local>
2020-04-11 19:06:16 -0400

    Updates

---
+++ precache-manifest.513fb4363b5553e2760b54c5c0830a7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439d49e669236c0330e8456b6c631fc8",
+    "url": "/potus-quotes/index.html"
+  },
+  {
+    "revision": "e8d6dcc24604b1986831",
+    "url": "/potus-quotes/static/css/main.99f64a75.chunk.css"
+  },
+  {
+    "revision": "28971310e5d10758bcc5",
+    "url": "/potus-quotes/static/js/2.3d6763d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/potus-quotes/static/js/2.3d6763d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d6dcc24604b1986831",
+    "url": "/potus-quotes/static/js/main.785d3b03.chunk.js"
+  },
+  {
+    "revision": "447fdddf6af9acd7853e",
+    "url": "/potus-quotes/static/js/runtime-main.a31679de.js"
+  },
+  {
+    "revision": "438059c9070ffb26deccda8189034c6e",
+    "url": "/potus-quotes/static/media/trump.438059c9.jpeg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/potus-quotes/precache-manifest.513fb4363b5553e2760b54c5c0830a7a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/potus-quotes/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
